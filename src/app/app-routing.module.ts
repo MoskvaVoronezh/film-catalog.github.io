1ceb65226c6830a5afe05ca1b1504b45c212dacf
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2020-07-30 00:56:28 +0700

    Роут для списка фильмов

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
+import { MoviesListComponent } from './pages/movies-list/movies-list.component';
 
 const routes: Routes = [
    {
       path: 'list',
+      component: MoviesListComponent
+   },
+   {
+      path: '**',
+      redirectTo: 'list'
    }
 ];
 
