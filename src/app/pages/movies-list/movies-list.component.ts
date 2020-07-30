ecb968dc412ca9725af06e2397391e7d5aa2afa8
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2020-07-31 00:38:49 +0700

    Поправки с выводом описания. Заготовка для вывода жанров

--- src/app/pages/movies-list/movies-list.component.ts
+++ src/app/pages/movies-list/movies-list.component.ts
@@ -14,6 +14,85 @@ export class MoviesListComponent implements OnInit {
   public apiUrl: string = environment.apiUrl;
   movies: any = [];
 
+  genres: [
+    {
+      id: 28,
+      name: "Action"
+    },
+    {
+      id: 12,
+      name: "Adventure"
+    },
+    {
+      id: 16,
+      name: "Animation"
+    },
+    {
+      id: 35,
+      name: "Comedy"
+    },
+    {
+      id: 80,
+      name: "Crime"
+    },
+    {
+      id: 99,
+      name: "Documentary"
+    },
+    {
+      id: 18,
+      name: "Drama"
+    },
+    {
+      id: 10751,
+      name: "Family"
+    },
+    {
+      id: 14,
+      name: "Fantasy"
+    },
+    {
+      id: 36,
+      name: "History"
+    },
+    {
+      id: 27,
+      name: "Horror"
+    },
+    {
+      id: 10402,
+      name: "Music"
+    },
+    {
+      id: 9648,
+      name: "Mystery"
+    },
+    {
+      id: 10749,
+      name: "Romance"
+    },
+    {
+      id: 878,
+      name: "Science Fiction"
+    },
+    {
+      id: 10770,
+      name: "TV Movie"
+    },
+    {
+      id: 53,
+      name: "Thriller"
+    },
+    {
+      id: 10752,
+      name: "War"
+    },
+    {
+      id: 37,
+      name: "Western"
+    }
+  ]
+
   ngOnInit(): void {
     this.movieService.getMovie().subscribe(movies => { 
       this.movies = movies.results;
@@ -24,4 +103,18 @@ export class MoviesListComponent implements OnInit {
   getFullImage(img) {
     return `https://image.tmdb.org/t/p/w500/${img}`; 
   }
+
+  // renderGenres(array) {
+  //   console.log(array);
+  //   let result = [];
+  //   // for(let i = 0; i < this.genres.length; i++) {
+  //   //   for(let j = 0; j < array.lenght; j++) {
+  //   //     this.genres[i].id == array[j];
+  //   //     console.log(this.genres[i].id == array[j]);
+  //   //     console.log(true);
+  //   //     result.push(this.genres[i].name);
+  //   //   }
+  //   // }
+  //   console.log(result);
+  // }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -10,28 +10,6 @@ export class MovieService {
   constructor(
     private http: HttpClient
   ) { }
-
-  genres = {
-    Action: 28,
-    Adventure: 12,
-    Animation: 16,
-    Comedy: 35,
-    Crime: 80,
-    Document: 99,
-    Drama: 18,
-    Family: 10751,
-    Fantasy: 14,
-    History: 36,
-    Horror: 27,
-    Music: 10402,
-    Mystery: 9648,
-    Romance: 10749,
-    SciFi: 878,
-    TVMovie: 10770,
-    Thriller: 53,
-    War: 10752,
-    Western: 37
-  }
     
   getMovie(): Observable<any> {
     return this.http.get(`https://api.themoviedb.org/3/discover/movie?api_key=ae2577fe39de40d905bc0b37e1523a8b&language=en-us&with_genres=`);
